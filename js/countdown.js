43135c45f4e7ccd614228d546df48ca7456fa7f2
R4C3R0P <54683866+instax-dutta@users.noreply.github.com>
2022-05-20 18:50:24 +0530

    Update countdown.js

--- js/countdown.js
+++ js/countdown.js
@@ -1,7 +1,7 @@
 var countDownDate = new Date();
     // Replace with desired date 'January 17, 2019 03:24:00', example
     // countDownDate.setDate('January 17, 2019 03:24:00').getTime();
-    countDownDate.setDate(countDownDate.getDate() + 11).getTime();
+    countDownDate.setDate('June 10, 2022').getTime();
     countDownDate.getTime();
 
     // Update the count down every 1 second
